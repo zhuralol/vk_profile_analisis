9e5c99879fe804a0991c96d895800470d0775aa7
DownedCrane <vovacrane@gmail.com>
2020-06-11 20:25:32 +0300

    todo - cytoscape.js todo - fix missing friends of friends in data todo - fix to overlapping nodes graph parse and build part

--- graphs.py
+++ graphs.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from random import randrange
 from ast import literal_eval as le
 
-USERID = 32953591
+USERID = 230377912
 
 def gen_rand_graph(G,id_counter,conn_counter):
     for i in range(0, id_counter):
@@ -21,7 +21,7 @@ def parsegraph(filename):
     kek = le(lines[0])
     return kek
 
-lol = parsegraph("32953591_graphdata.txt")
+lol = parsegraph(str(USERID)+"_graphdata.txt")
 print(lol[1][0])
 graphedges = []
 for link in lol[0]:
@@ -31,8 +31,27 @@ for link in lol[0]:
 
 
 print(graphedges)
+
 G = nx.from_edgelist(graphedges)
-G = nx.k_core(G, k=3)
+
+# G = nx.Graph()
+
+labels = []
+nx.set_node_attributes(G, labels, 'labels')
+
+for node in G:
+    for ent in lol[1]:
+        if node == ent["id"]:
+            print(node)
+            G.nodes[node]['labels'] = ent['label']
+    # print(node)
+    # print("lel")
+    # print(lol[1][1])
+    # node['labels']=lol[1][node]['label']
+    # print(lol[1])
+    # print(node["id"])
+
+# G = nx.k_core(G, k=2)
 
 #G = nx.karate_club_graph()
 #G = nx.random_powerlaw_tree(50, gamma=4, seed=None, tries=10000)
@@ -54,14 +73,14 @@ print(cliq)
 
 
 
-nbrs = nx.all_neighbors(G, str(USERID))
+#nbrs = nx.all_neighbors(G, str(USERID))
 # color_map=[]
 # for node in G:
 #     print(node)
 #     color_map[int(node)]="red"
 
 # nx.draw(G, node_color=color_map, with_labels=True)
-nx.write_gexf(G, "test.gexf")
+nx.write_gexf(G, str(USERID)+"test.gexf")
 nx.draw(G, with_labels=True)
-
+#
 plt.show()
