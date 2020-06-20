8fb1418d98e1b775948e2fd45c70d3a3e9f87929
DownedCrane <vovacrane@gmail.com>
2020-06-20 14:54:12 +0300

    added database models from SQLAlchemy updated req

--- main.py
+++ main.py
@@ -13,14 +13,51 @@ from flask import Flask, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask import render_template
 
-# mysqlite
+# database imports
+from flask_sqlalchemy import SQLAlchemy
 
-# creating application and bootstrapping it
+
+# creating application, database and bootstrapping it
 app = Flask(__name__)
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
-# turn off on production!
+# database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+
+# debug status - turn off on production!
 app.config['DEBUG'] = True
+
+# database models
+# creating user class for auth and billing
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # delete image after
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    # thing to properly print this class
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+# creating dummy post class for saving info from queries
+# todo rename this
+class Post(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    # thing to properly print this class
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
 
 # create API to use
 # TODO - поменять scope
