1e255417bce0cf6f68567474fb7c92bb8729cbe5
DownedCrane <vovacrane@gmail.com>
2022-06-10 15:54:30 +0300

    Update auth.py

--- zhuraapp/auth.py
+++ zhuraapp/auth.py
@@ -1,8 +1,8 @@
 # id of vk.com application
-APP_ID = 5745809
-APP_LOGIN = "89167812455"
-APP_PASSWORD = "?real_woman223522?"
+APP_ID = 123
+APP_LOGIN = ""
+APP_PASSWORD = "??"
 
-APP_SECRET_TOKEN = "b2b79eeab2b79eeab2b79eea3ab2e0327bbb2b7b2b79eeae8bd72120411e9ecb7922acf"
-# APP_LOGIN = "89967897971"
-# APP_PASSWORD = "NewRevolution"
+APP_SECRET_TOKEN = ""
+# APP_LOGIN = ""
+# APP_PASSWORD = ""
